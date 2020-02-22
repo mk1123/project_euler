ea11f45cbdcf94cc7f459e6f3dd3cbc6818a839d
Manan Khattar <khattar.manan@gmail.com>
2020-02-22 18:42:30 +0000

    up to 20

--- 020.py
+++ 020.py
@@ -8,3 +8,17 @@ Find the sum of the digits in the number 100!
 """
 
 
+def digit_sum(n):
+    r = 0
+    while n:
+        r, n = r + n % 10, n // 10
+    return r
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+
+
+print(digit_sum(factorial(100)))
