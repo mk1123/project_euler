dffce85764a1426a09c64cc036fe1f59ca8e6be0
Manan Khattar <khattar.manan@gmail.com>
2020-02-22 16:34:03 +0000

    up to 14

--- 004.py
+++ 004.py
@@ -7,5 +7,14 @@ from the product of two 2-digit numbers is 9009 = 91 * 99.
 
 Find the largest palindrome made from the product of two 3-digit numbers.
 """
+max_pal = 0
 
+for i in range(999, 99, -1):
+    for j in range(i, 99, -1):
+        mult = i * j
+        if str(mult) == str(mult)[::-1]:
+            if mult > max_pal:
+                max_pal = mult
+                break
 
+print(max_pal)
