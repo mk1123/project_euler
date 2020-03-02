24a5669d154fb077debf095d1d491521b29152ce
Manan Khattar <khattar.manan@gmail.com>
2020-03-02 13:15:22 +0000

    first 25

--- 024.py
+++ 024.py
@@ -13,4 +13,18 @@ What is the millionth lexicographic permutation of the digits 0, 1, 2, 3,
 4, 5, 6, 7, 8 and 9?
 """
 
+from math import factorial
+
+digits = list(range(10))
+num = 1000000 - 1
+count = 10
+answer_digits = []
+
+while digits:
+    next_digit = digits[int(num % factorial(count) / factorial(count - 1))]
+    answer_digits.append(next_digit)
+    digits.remove(next_digit)
+    count -= 1
+
+print("".join(map(str, answer_digits)))
 
---
+++ 025.py
@@ -0,0 +1,43 @@
+"""
+Project Euler Problem 25
+========================
+
+The Fibonacci sequence is defined by the recurrence relation:
+
+  F[n] = F[n[1]] + F[n[2]], where F[1] = 1 and F[2] = 1.
+
+Hence the first 12 terms will be:
+
+  F[1] = 1
+  F[2] = 1
+  F[3] = 2
+  F[4] = 3
+  F[5] = 5
+  F[6] = 8
+  F[7] = 13
+  F[8] = 21
+  F[9] = 34
+  F[10] = 55
+  F[11] = 89
+  F[12] = 144
+
+The 12th term, F[12], is the first term to contain three digits.
+
+What is the first term in the Fibonacci sequence to contain 1000 digits?
+"""
+from math import log10
+
+digits = lambda n: int(log10(n)) + 1
+
+a = 0
+b = 1
+c = 1
+count = 1
+
+while digits(c) != 1000:
+    c = a + b
+    a, b = b, c
+    count += 1
+
+print(count)
+
