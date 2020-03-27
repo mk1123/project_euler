31e6c1514bd24139bc20db5b6e02e9dc1f7fd80b
Manan Khattar <khattar.manan@gmail.com>
2020-03-27 00:29:58 -0700

    till 38

--- 037.py
+++ 037.py
@@ -14,12 +14,38 @@ NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
 """
 
 from utils import gen_primes
+from math import log10
+
+
+def valid_property(n):
+    copy_n = n
+    while copy_n:
+        if copy_n not in primes:
+            break
+        copy_n //= 10
+    if copy_n > 0:
+        return False
+
+    digits = int(log10(n))
+    while n:
+        if n not in primes:
+            break
+        n %= 10 ** digits
+        digits -= 1
+    return n == 0
 
 
 primes = set()
 
-start_double_digit_primes = {23, 25, 29, 31, 35, 37, 53, 59, 71, 73, 79}
-end_double_digit_primes = {}
+counter = 0
+sum_ = 0
 
 for prime in gen_primes():
     primes.add(prime)
+    if prime > 10 and valid_property(prime):
+        counter += 1
+        sum_ += prime
+        if counter == 11:
+            break
+
+print(sum_)
