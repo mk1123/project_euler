4c30d373e4aa593df04970908d2d4e0fc166390d
Manan Khattar <khattar.manan@gmail.com>
2021-01-05 15:36:14 -0800

    63

--- 063.py
+++ 063.py
@@ -7,5 +7,13 @@ The 5-digit number, 16807=7^5, is also a fifth power. Similarly, the
 
 How many n-digit positive integers exist which are also an nth power?
 """
+from math import log10, ceil
 
+total_sum = 0
 
+for i in range(1, 30):
+    for j in range(1, 10):
+        if ceil(log10(j ** i)) == i:
+            total_sum += 1
+
+print(total_sum + 1)  # doesn't include 1 because of log stuff
