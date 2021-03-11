b0dfbea8b28af2571e3fc65ce496d9a94dc9da62
Manan Khattar <khattar.manan@gmail.com>
2021-03-10 21:00:20 -0800

    at no 68

--- 066.py
+++ 066.py
@@ -28,4 +28,67 @@ Find the value of D 1000 in minimal solutions of x for which the largest
 value of x is obtained.
 """
 
+from math import sqrt
+from typing import Generator, Tuple
+
+
+def cont_fraction_rep_of_root(num: int) -> Generator[int, None, None]:
+    root = sqrt(num)
+    limit = int(root)
+    if root == limit:
+        yield -1
+        return
+    yield limit
+    while True:
+        d = 1
+        m = 0
+        a = limit
+        while a != 2 * limit:
+            m = d * a - m
+            d = (num - m * m) // d
+            a = (limit + m) // d
+            yield a
+
+
+def convergents_of_root(num: int) -> Generator[Tuple[int, int], None, None]:
+    cont_frac_reps = cont_fraction_rep_of_root(num)
+    p = next(cont_frac_reps)
+    q = 1
+    prev_prev = (p, q)
+    yield prev_prev
+    a1 = next(cont_frac_reps)
+    p = p * a1 + 1
+    q = a1
+    prev = (p, q)
+    yield prev
+    for i in cont_frac_reps:
+        curr = (
+            i * prev[0] + prev_prev[0],
+            i * prev[1] + prev_prev[1],
+        )
+        yield curr
+        prev_prev = prev
+        prev = curr
+
+
+def min_x_pells(D: int) -> int:
+    convergents = convergents_of_root(D)
+    for p, q in convergents:
+        if p ** 2 - D * q ** 2 == 1:
+            return p
+
+    return -1
+
+
+max_D = 0
+max_X = 0
+
+for D in range(1, 1001):
+    if int(sqrt(D)) != sqrt(D):
+        x = min_x_pells(D)
+        if x > max_X:
+            max_X = x
+            max_D = D
+
+print(max_D)
 
