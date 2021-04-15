83a996c816de4054a5b6e2386d4452627837d83c
Manan Khattar <khattar.manan@gmail.com>
2021-04-15 03:07:11 -0700

    overcomplicated counting change but figured it out

--- 076.py
+++ 076.py
@@ -15,4 +15,23 @@ How many different ways can one hundred be written as a sum of at least
 two positive integers?
 """
 
+num = 100
+
+dp = [[0] * (num + 1) for _ in range(num + 1)]
+
+for n in range(num + 1):
+    dp[0][n] = 1
+    dp[n][1] = 1
+
+for n in range(2, num + 1):
+    for k in range(2, n):
+        return_sum = 0
+        if (n - k) <= k:
+            return_sum += 1 + dp[n - k][n - k - 1]
+        else:
+            return_sum += dp[n - k][k]
+        return_sum += dp[n][k - 1]
+        dp[n][k] = return_sum
+
+print(dp[num][num - 1])
 
